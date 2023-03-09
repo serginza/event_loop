68463be3c2174f26fca9052c1f0e0c04027e9d1f
Sergey <polikarpov-sereg@mail.ru>
2023-03-09 13:40:39 +0300

    feat: complete task #2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // Task #1
+console.log("Task #1(sum through closure):")
+
 function func(a) {
     return (b) => {
         return (c) => {
@@ -8,18 +10,51 @@ function func(a) {
 }
 
 console.log(func(4)(5)(6));
+console.log("__________________________")
 
 // Task #2
+console.log("Task #2(quick sort):")
 
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
 
+    let less =[];
+    let more = [];
 
+    const index = Math.floor(arr.length / 2);
+    const currentValue = arr[index];
 
+    for (let i = 0; i < arr.length; i++) {
+        if (i === index) {
+            continue
+        }
+        if (arr[i] < currentValue ) {
+            less.push(arr[i])
+        } else {
+            more.push(arr[i])
+        }
+    }
 
+    return [ 
+        ...quickSort(less), 
+        currentValue, 
+        ...quickSort(more) 
+    ];
+}
 
+const array = [4, 68, 91, 14, 364];
+const array2 = [4, 68, 91, 14, 364, 34, 144, 99, 104, 53, 87, 777, 23, 533, 11, 7];
 
+console.log(array);                // original massive
+console.log(quickSort(array));     // sort massive
 
+console.log(array2);                 // original massive2
+console.log(quickSort(array2));      // sort massive2
 
-
+console.log("__________________________")
 
 // Task #3
+console.log("Task #3:")
 
