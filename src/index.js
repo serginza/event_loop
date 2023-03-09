420997f29fcf3e36faf4df3f4f6ab41915368b7d
Sergey <polikarpov-sereg@mail.ru>
2023-03-09 11:06:14 +0300

    feat: complete task #1

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Task #1
+function func(a) {
+    return (b) => {
+        return (c) => {
+            return a + b + c;
+        };
+    };
+}
+
+console.log(func(4)(5)(6));
+
+// Task #2
+
+
+
+
+
+
+
+
+
+
+
+// Task #3
+
