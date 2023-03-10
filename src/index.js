54982f6b0752149d4c3d4cc5cdfbabb139507789
Sergey <polikarpov-sereg@mail.ru>
2023-03-10 14:08:13 +0300

    feat: complete task #3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 // Task #1
+
 console.log("Task #1(sum through closure):")
+
+//Решение с неограниченным количеством аргументов
+// function func(a) {
+//     return function(b) {
+//         return b ? func(a + b) : a;
+//     }
+// }
 
 function func(a) {
     return (b) => {
@@ -9,7 +17,7 @@ function func(a) {
     };
 }
 
-console.log(func(4)(5)(6));
+console.log("Sum(2)(3)(5) = " + func(2)(3)(5));  // 10
 console.log("__________________________")
 
 // Task #2
@@ -29,13 +37,13 @@ function quickSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (i === index) {
             continue
-        }
+        };
         if (arr[i] < currentValue ) {
             less.push(arr[i])
         } else {
             more.push(arr[i])
-        }
-    }
+        };
+    };
 
     return [ 
         ...quickSort(less), 
@@ -58,3 +66,54 @@ console.log("__________________________")
 // Task #3
 console.log("Task #3:")
 
+//функция присваивания действия кнопки для каждого пункта задания
+function button(func, btn) {
+    document.addEventListener("click", (e) => {
+        let el = e.target.closest(btn);
+        if (el) {
+            func();
+            document.querySelector(btn).setAttribute('disabled', 'true'); 
+        };
+    });
+}
+//Первый пункт задания
+function microtask1() {
+        queueMicrotask(() => {
+            console.log("1_Microtask done!")
+            //alert("Microtask done!");   
+        });
+
+        setTimeout(() => {
+            document.body.style.background = "aqua";
+            document.getElementsByClassName("button3").innerText = "Ура! Кнопка переименована после вполнения микрозадачи!";
+            console.log("1_Body was recolored!");
+        }, 0)
+}
+
+//Второй пункт задания
+function microtask2() {
+    queueMicrotask(() => {
+        console.log("2-1_Microtask done!")
+    });
+
+    queueMicrotask(() => {
+        console.log("2-2_Microtask done!")
+    });
+}
+
+//Третий пункт задания
+function microtask3() {
+    queueMicrotask(() => {
+        console.log("3_Microtask done!") 
+    });
+
+    setTimeout(() => {
+        document.querySelector(".button3").textContent = "Ура! Кнопка переименована после вполнения микрозадачи!"
+        console.log("3_Button was renamed!");
+    }, 0)
+}
+
+//Кнопки для выполнения каждого пункта задания
+button(microtask1, ".button1");
+button(microtask2, ".button2");
+button(microtask3, ".button3");
